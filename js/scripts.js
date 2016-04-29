0575186091e90c7055671436fafaf967d6682070
kmytrofanov <ettelenzp@gmail.com>
2016-04-29 20:46:18 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var myApp = angular.module("myModule", [])
+                   .controller("myController", function ($scope, $http) {
+    $scope.message = "Table Header";
+    //$scope.message = $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) { $scope.message = response.data; });
+    var base64EncodedString = btoa('986efe8785747c87fd2d93e66e0058ae268894bf9769e88ebeec38cd3e35ef71');
+    $http.defaults.headers.common.Authorization = 'Basic ' + base64EncodedString;
+    $scope.elements = $http.get('https://api.kumulos.com/v1/data/6205_6210_users')
+        .then(function (response) {
+            $scope.elements = response.data;
+        });
+});
