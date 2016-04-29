5f3a55a8129c5156b2623fe315fe0b13e25a633d
kmytrofanov <ettelenzp@gmail.com>
2016-04-29 23:11:52 +0100

    fixed config.xml

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var myApp = angular.module("myModule", [])
                    .controller("myController", function ($scope, $http) {
+                       
     $scope.message = "Table Header";
     var base64EncodedString = btoa('986efe8785747c87fd2d93e66e0058ae268894bf9769e88ebeec38cd3e35ef71');
     $http.defaults.headers.common.Authorization = 'Basic ' + base64EncodedString;
@@ -9,21 +10,3 @@ var myApp = angular.module("myModule", [])
             $scope.full_message = response;
         });
 });
-
-function checkConnection() {
-    var networkState = navigator.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    alert('Connection type: ' + states[networkState]);
-}
-
-checkConnection();
