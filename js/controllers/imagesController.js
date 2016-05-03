9b9947f04e1c959750e8a579aa2481930092e749
= <=>
2016-05-03 15:26:32 +0100

    Added encoding for images

--- js/controllers/imagesController.js
+++ js/controllers/imagesController.js
@@ -4,20 +4,36 @@ var imagesController = function ($scope, dataStorage) {
     $scope.$parent.mainPage = false;
     $scope.$parent.canGoBack = true;
     
-    dataStorage.getImages().then(function (response) { $scope.images = response.data; });
+    dataStorage.getImages().then(function (response) {
+        $scope.images = response.data;
+        for (var i = 0; i < $scope.images.length; i++){
+            $scope.images[i].file = atob($scope.images[i].file);
+        }
+    });
     
     $scope.addNewImage = function(image){
-        var newImage = {"name":"NewFile2", "file":image};
+        var encodedImage = btoa(image);
+        var newImage = {"name":"NewFile2", "file":encodedImage};
         dataStorage.addNewImage(newImage)
             .then(function(response) {
-                dataStorage.getImages().then(function (response) { $scope.images = response.data; });
+                dataStorage.getImages().then(function (response) {
+                    $scope.images = response.data;
+                    for (var i = 0; i < $scope.images.length; i++){
+                        $scope.images[i].file = atob($scope.images[i].file);
+                    }
+                });
             });
     };
     
     $scope.deleteImage = function(id){
         dataStorage.deleteImage(id)
             .then(function(response) {
-                dataStorage.getImages().then(function (response) { $scope.images = response.data; });
+                dataStorage.getImages().then(function (response) {
+                    $scope.images = response.data;
+                    for (var i = 0; i < $scope.images.length; i++){
+                        $scope.images[i].file = atob($scope.images[i].file);
+                    }
+                });
             });
     };
 };
